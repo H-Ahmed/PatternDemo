0896568f2656e310f45e52e42120f7fc04edc223
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-01-05 20:27:22 +0200

    Creational Patterns Singleton Pattern

--- src/com/company/AbstractFactory.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public abstract class AbstractFactory {
-    abstract Color getColor(String color);
-    abstract Shape getShap(String shap);
-}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,28 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
-
-        Shape shape1 = shapeFactory.getShap("CIRCLE");
-        shape1.draw();
-
-        Shape shape2 = shapeFactory.getShap("RECTANGLE");
-        shape2.draw();
-
-        Shape shape3 = shapeFactory.getShap("SQUARE");
-        shape3.draw();
-
-
-        AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
-
-        Color color1 = colorFactory.getColor("RED");
-        color1.fill();
-
-        Color color2 = colorFactory.getColor("GREEN");
-        color2.fill();
-
-        Color color3 = colorFactory.getColor("BLUE");
-        color3.fill();
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
 
     }
 }
